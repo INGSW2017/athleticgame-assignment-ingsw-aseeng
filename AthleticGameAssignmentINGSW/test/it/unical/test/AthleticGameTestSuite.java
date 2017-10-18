c558cdf304bedf3cc7fc74e901b0fd7fd5846b04
aseeng <alessandromarrazzo9@gmail.com>
2017-10-18 13:01:10 +0200

    modifica

--- AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameTestSuite.java
+++ AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameTestSuite.java
@@ -1,14 +1,10 @@
 package it.unical.test;
 
-
-
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@SuiteClasses({AthleticGameWinnersTest.class, AthleticGameBasicTest.class})
+@SuiteClasses({ AthleticGameWinnersTest.class, AthleticGameBasicTest.class })
 public class AthleticGameTestSuite {
-	
 }
